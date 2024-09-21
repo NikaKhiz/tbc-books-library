5718ccf9da14afce1128a059525832798fddd418
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-21 23:20:58 +0400

    books with most pages feature

--- database_manager.py
+++ database_manager.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+import json
 from utils import *
 from models import Author 
+
 
 class DatabaseManager():
     def __init__(self, engine, base) -> None:
@@ -16,9 +19,30 @@ class DatabaseManager():
         self.session.add_all(authors)
         self.session.commit()
 
+
     def popuate_book_table(self):
         authors = self.session.query(Author.id, Author.birth_date).all()
         books = [generate_random_book(authors) for _ in range(1000)]
         self.session.add_all(books)
         self.session.commit()
+
+
+    def books_with_most_pages(self):
+        # check for largest num of pages from book, for filtering
+        max_pages = self.session.query(func.max(Book.pages)).scalar()
+        books = self.session.query(Book).filter(Book.pages == max_pages).all()
+
+        books_list = [
+            {
+                'id': book.id,
+                'title': book.title,
+                'pages': book.pages,
+                'release_date': book.release_date.isoformat(),
+                'author_id': book.author_id
+            }
+            for book in books
+            ]
+        
+        return json.dumps(books_list, indent=4)
+
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     db.popuate_author_table()
     db.popuate_book_table()
 
+    print(f'books with most pages {db.books_with_most_pages()}')
 
 if __name__ == '__main__':
     main()
